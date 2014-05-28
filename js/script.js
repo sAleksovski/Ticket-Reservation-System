6f5321bbce42740cec0b9c0db2ac71a59503ab9f
sAleksovski <aleksovski.stefan994@gmail.com>
2014-05-28 02:17:04 +0200

    Added results view

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function initialize() {
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 	google.maps.event.addListener(map, 'click', function(event) {
-    	//alert(event.latLng);
 		var input = String(event.latLng);
 		input = input.replace("(","");
 		input = input.replace(")","");
@@ -27,46 +26,46 @@ function initialize() {
     	var geocoder = new google.maps.Geocoder();
     	geocoder.geocode({'latLng': latlng}, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK && results[1]) {
-					// user clicked on US and return result is in accepted format
-    				if(results[1].address_components[results[1].address_components.length - 1].short_name == "US" &&
-    					results[1].address_components[results[1].address_components.length - 2].short_name != "US") {
+				// user clicked on US and return result is in accepted format
+				if(results[1].address_components[results[1].address_components.length - 1].short_name == "US" &&
+					results[1].address_components[results[1].address_components.length - 2].short_name != "US") {
 
-    					//Fill #from field
-    					if (markerFrom == null) {
-	    					markerFrom = new google.maps.Marker({
-       						 	position: event.latLng, 
-        						map: map,
-        						title: "Click to remove"
-    						});
-							$("#from").val(results[1].address_components[results[1].address_components.length - 2].long_name);
+					//Fill #from field
+					if (markerFrom == null) {
+    					markerFrom = new google.maps.Marker({
+   						 	position: event.latLng, 
+    						map: map,
+    						title: "Click to remove"
+						});
+						$("#from").val(results[1].address_components[results[1].address_components.length - 2].long_name);
 
-							google.maps.event.addListener(markerFrom, 'click', function() {
-								removeMarker(markerFrom);
-							});
+						google.maps.event.addListener(markerFrom, 'click', function() {
+							removeMarker(markerFrom);
+						});
 
-							if (markerTo != null) {
-								drawPath();
-							};
-						}
-
-						// Fill #to field and draw  path
-						else if (markerTo == null) {
-	    					markerTo = new google.maps.Marker({
-       						 	position: event.latLng, 
-        						map: map,
-        						title: "Click to remove"
-    						});
-							$("#to").val(results[1].address_components[results[1].address_components.length - 2].long_name);
-
-							google.maps.event.addListener(markerTo, 'click', function() {
-								removeMarker(markerTo);
-							});
-
-							if (markerFrom != null) {
-								drawPath();
-							};
-						}
+						if (markerTo != null) {
+							drawPath();
+						};
 					}
+
+					// Fill #to field and draw  path
+					else if (markerTo == null) {
+    					markerTo = new google.maps.Marker({
+   						 	position: event.latLng, 
+    						map: map,
+    						title: "Click to remove"
+						});
+						$("#to").val(results[1].address_components[results[1].address_components.length - 2].long_name);
+
+						google.maps.event.addListener(markerTo, 'click', function() {
+							removeMarker(markerTo);
+						});
+
+						if (markerFrom != null) {
+							drawPath();
+						};
+					}
+				}
 			}
 		});
 	});
@@ -96,37 +95,7 @@ function drawPath () {
 google.maps.event.addDomListener(window, 'load', initialize);
 // MAPA KRAJ
 
-// FLIP
-var init = function() {			  
-  $('#flip').click(function(){
-  		/*var a = $(".ad-card-text").css("opacity");
-  		a = a.split('.');
-  		if (a[0] == 0) {
-			$(".ad-card-text").css("opacity", "1.0");
-			$("#flip-card").toggleClass('flipped');
-		} else {
-			$("#flip-card").toggleClass('flipped');
-			setTimeout(function(){$(".ad-card-text").css("opacity", "0.8")}, 1000);
-		};*/
-		if ($("#flip-card").hasClass('flipped')) {
-			flip(false);
-		}
-		else {
-			flip(true); 
-		};
-	});
-};
-
-window.addEventListener('DOMContentLoaded', init, false);
-// FLIP KRAJ
-
-
 $(function() {
-	$("#test").click(function() {
-		alert(contains("Alaska"));
-		alert(contains("dasdasds"));	
-	}); 
-
 	$( "#from" ).autocomplete({
 		minLength: 0,
 		source: stateCoor,
@@ -204,25 +173,19 @@ $(function() {
 
 	$("#from").focusin(function() {
 		if (! $("#flip-card").hasClass("flipped")) {
-			//$("#flip-card").addClass('flipped');
 			flip(true);
 		};
-		
-
 	});
 
 	$("#from").focusout(function() {
 		if ($("#from").val().trim().length == 0 && $("#to").val().trim().length == 0 &&
 			!$("#flip-card").is(":hover") && ! $("#to").is(":hover") ) {
 			flip(false);
-			//$("#flip-card").removeClass('flipped');
-
 		};		
 	});
 
 	$("#to").focusin(function() {
 		if (! $("#flip-card").hasClass("flipped")) {
-			//$("#flip-card").addClass('flipped');
 			flip(true);
 		};
 	});
@@ -230,10 +193,10 @@ $(function() {
 	$("#to").focusout(function() {
 		if ($("#from").val().trim().length == 0 && $("#to").val().trim().length == 0 &&  ! $("#from").is(":hover")
 			&& ! $("#flip-card").is(":hover") ) {
-			//$("#flip-card").removeClass('flipped');
 			flip(false);
 		};
 	});
+
 	$("#date").datepicker({ minDate: 0, maxDate: "+1Y" });
 	$("#tabs").tabs();
 
@@ -242,9 +205,9 @@ $(function() {
 function contains (target) {
 	var res = false;
 	$.each(stateCoor, function(i, item) {
-    		if (target.toString().trim() == item.label.toString().trim()) {
-    			res = true;
-    		};
+		if (target.toString().trim() == item.label.toString().trim()) {
+			res = true;
+		};
 	})
 	return res;
 }
@@ -267,7 +230,6 @@ function flip (addClass) {
 	if (addClass) {
 		$(".ad-card-text").css("opacity", "1.0");
 		$("#flip-card").addClass('flipped');
-		//return;
 	} else {
 		$("#flip-card").removeClass('flipped');
 		setTimeout(function(){$(".ad-card-text").css("opacity", "0.8")}, 1000);
